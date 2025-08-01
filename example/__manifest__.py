8c08bc26d4fcc1e451b506ed6b200a735215ec1a
yusuke1998 <jhonnyjose1998@gmail.com>
2025-08-01 17:22:04 -0400

    test 2

--- example/__manifest__.py
+++ example/__manifest__.py
@@ -7,7 +7,7 @@
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose 1
+        Long description of module's purpose 2
     """,
 
     'author': "Yusuke1998",
